a032ab66148140871ad79e35537ee13a1d97621b
jcy129815 <130085244+jcy129815@users.noreply.github.com>
2023-07-30 03:22:41 +0900

    Update js.js

--- script/js.js
+++ script/js.js
@@ -9,5 +9,8 @@ console.log(userHeight, userWeight, normal_w, user_text, user_btn)
 //식
 normal_w = (userHeight-100)*0.9; // 63
 bad_w = normal_w - userWeight; // -5
-// console.log(normal_w, bad_w) 
 console.log(`철수의 적정 체중은${normal_w}kg이며 초과된 kg은 ${bad_w}kg이다`)
+// console.log(normal_w, bad_w) 
+user_btn.addEventListener('click',function(){
+    user_text.value = `철수의 적정 체중은${normal_w}kg이며 초과된 kg은 ${bad_w}kg이다`
+})
